61ac762df131310babb5820acb56eedbb90d9306
ekansrm <ekansrm0001@163.com>
2018-06-20 08:12:09 +0800

    CorpusContentExtraction

--- src/main/java/app/CorpusContentExtraction.java
+++ src/main/java/app/CorpusContentExtraction.java
@@ -1,16 +1,18 @@
 package app;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.util.regex.Pattern.*;
+
 
 /**
  * @author weiming
@@ -21,58 +23,100 @@ public class CorpusContentExtraction {
   /**
    * 从XML语料提取正文部分
    */
-  void extractorXML(Document doc, FileOutputStream out) {
+  void extractorXML(FileInputStream in, FileOutputStream out) throws ParserConfigurationException, IOException {
+    DocumentBuilderFactory builderFactory =  DocumentBuilderFactory.newInstance();
+
+    DocumentBuilder builder = builderFactory.newDocumentBuilder();
+
+    Document doc;
+
+    // 实例化InputStreamReader时，已指定字符编码
+    BufferedReader reader = new BufferedReader(new InputStreamReader(in, "GBK"));
+
+
+    Pattern pattern = compile("(?<=</doc>)");
+
+    String line;
+    StringBuilder contentBuilder = new StringBuilder();
+    while((line=reader.readLine())!=null) {
+      contentBuilder.append(line);
+    }
+
+    for(String s: pattern.split(contentBuilder.toString())) {
+
+      try {
+        System.out.println("====");
+        InputStream is = new ByteArrayInputStream(s.getBytes("UTF-8"));
+        doc = builder.parse(is);
+
+
+        NodeList nList = doc.getElementsByTagName("content");
+
+        for(int  i = 0 ; i<nList.getLength();i++) {
+          System.out.println(nList.item(i).getTextContent());
+        }
+
+      } catch (Exception e) {
+        e.printStackTrace();
+
+      }
+
+    }
+
+  }
+
+
+  static Pattern excapePattern = compile("[\uE40C]");
+  static public String excape(String line) {
+    Matcher matcher = excapePattern.matcher(line);
+    String result = matcher.replaceAll(" ");
+    return result;
+  }
+
+  static Pattern headPattern = compile("^.*?（记者.*?）|日(电|消息)");
+
+  static public String removeNoValue(String line) {
+    Matcher matcher = headPattern.matcher(line);
+    return matcher.replaceAll("");
+  }
+
+  static public void extractorSogouNewContentFast(FileInputStream in, FileOutputStream out) throws IOException {
+
+    Pattern pattern = compile("(?<=</doc>)");
+    Pattern contentPattern = compile("<content>(.*?)</content>");
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(in, "GBK"));
+
+
+    StringBuilder contentBuilder = new StringBuilder();
+    String line;
+    while((line=reader.readLine())!=null) {
+      contentBuilder.append(line);
+      if(pattern.matcher(line).find()) {
+        Matcher matcher = contentPattern.matcher(contentBuilder.toString());
+        if(matcher.find()) {
+          String content = matcher.group(1);
+          content = excape(content);
+          content = removeNoValue(content);
+          if(content.length()==0) {
+//            continue;
+          }
+          System.out.println(content);
+        }
+        contentBuilder = new StringBuilder();
+      }
+    }
+
+
+
+
+
 
   }
 
   static public void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
-
-    // 实例化InputStreamReader时，已指定字符编码
-    BufferedReader reader = new BufferedReader(
-      new InputStreamReader(
-        new FileInputStream("src/main/resources/news_tensite_xml.smarty.dat"), "GBK"));
-    InputSource is = new InputSource(reader);
-
-
-      DocumentBuilderFactory builderFactory =  DocumentBuilderFactory.newInstance();
-
-      DocumentBuilder builder = builderFactory.newDocumentBuilder();
-
-      Document doc = builder.parse(is);
-
-      doc.getDocumentElement().normalize();
-
-      System.out.println("Root  element: "+doc.getDocumentElement().getNodeName());
-
-      NodeList nList = doc.getElementsByTagName("book");
-
-      for(int  i = 0 ; i<nList.getLength();i++){
-
-        Node node = nList.item(i);
-
-        System.out.println("Node name: "+ node.getNodeName());
-        Element ele = (Element)node;
-
-        System.out.println("----------------------------");
-        if(node.getNodeType() == Element.ELEMENT_NODE){
-
-          System.out.println("book category: "+ ele.getAttribute("category"));
-
-          System.out.println("title name: "+ ele.getElementsByTagName("title").item(0).getTextContent());
-
-          System.out.println("author name: "+ele.getElementsByTagName("author").item(0).getTextContent());
-
-          System.out.println("year :"+ele.getElementsByTagName("year").item(0).getTextContent());
-
-          System.out.println("price : "+ele.getElementsByTagName("price").item(0).getTextContent());
-
-          System.out.println("-------------------------");
-
-
-        }
-
-
-      }
+      FileInputStream in = new FileInputStream("src/main/resources/news_tensite_xml.smarty.dat");
+      extractorSogouNewContentFast(in, null);
 
     }
 
